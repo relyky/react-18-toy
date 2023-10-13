43b14966eb6ee7a19f688db9de83351775e6c9a4
Rely Ky <relyky@hotmail.com>
2023-10-13 18:34:18 +0800

    NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import { createHashRouter,RouterProvider } from 'react-router-dom'
-import { Container, Typography, Button } from '@mui/material'
-import HomePage from './views/Home/HomePage'
-import MainOutlet from './views/MainOutlet'
+import HomePage from 'views/Home/HomePage'
+import MainOutlet from 'views/MainOutlet'
+import NotFound from 'views/NotFound'
 import About from 'views/About/About'
 import VisNetwork from 'views/VisNetwork/AppForm'
-import type { FC } from 'react'
-
-const NotFound: FC = () => {
-  return (
-    <Container>
-      <Typography variant='h1'>404 NotFound</Typography>
-      <Typography variant='h2'>Your Princess Is In Another Castle!</Typography>
-      <Button href="#/" variant="contained">回首頁</Button>
-    </Container>
-  )
-}
 
 //※gh-pages 不支援 BrowserRouter 故採用 HashRouter。
 const router = createHashRouter([
---
+++ src/views/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Typography, Button } from '@mui/material'
+import type { FC } from 'react'
+
+const NotFound: FC = () => {
+  return (
+    <Container>
+      <Typography variant='h1'>404 NotFound</Typography>
+      <Typography variant='h2'>Your Princess Is In Another Castle!</Typography>
+      <Button href="#/" variant="contained">回首頁</Button>
+    </Container>
+  )
+}
+
+export default NotFound;
