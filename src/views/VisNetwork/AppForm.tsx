792124f79badb7351a3d36b611b1bff10ee71de2
Rely Ky <relyky@hotmail.com>
2023-10-13 20:14:32 +0800

    vis-network basic sample

--- src/views/VisNetwork/AppForm.tsx
+++ src/views/VisNetwork/AppForm.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react"
 import { Container, Typography } from '@mui/material'
+import { Network } from "vis-network"
 
 export default function VisNetwork_AppForm() {
+  const visRef = useRef<HTMLDivElement>(null);
+
+  const nodes = [
+    { id: 1, label: "Node 1", title: "tool tip 1" },
+    { id: 2, label: "節點２", title: "tool tip 2" },
+    { id: 3, label: "Node 3", title: "tool tip 3" },
+    { id: 4, label: "節點四", title: "tool tip 4" },
+    { id: 5, label: "Node 5", title: "tool tip 5" },
+    { id: 6, label: "第六點", title: "tool tip 6" },
+  ];
+
+  const edges = [
+    { from: 1, to: 3 },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 },
+    { from: 1, to: 5 },
+  ];
+
+  const data = {
+    nodes: nodes,
+    edges: edges,
+  };
+
+  const options = {
+    height: "400px",
+    //layout: {
+    //  hierarchical: true
+    //},
+    //edges: {
+    //  color: "#000000"
+    //},
+  };
+
+  useEffect(() => {
+    const network = new Network(visRef.current!, data, options);
+    // Use `network` here to configure events, etc
+  }, [visRef, nodes, edges, options]);
+
   return (
-    <Container>
-        <Typography variant='h1'>VisNetwork</Typography>
-    </Container>
+    <div>
+      <h4>VisNetworkLab1</h4>
+      <div ref={visRef} style={{
+        border: 'solid 1px darkgrey'
+      }} />
+    </div>
   )
 }
+
+{/* <Container>
+<Typography variant='h1'>VisNetwork</Typography>
+</Container> */}
