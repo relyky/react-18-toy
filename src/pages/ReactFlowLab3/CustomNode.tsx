7e49725563951965f9f4ee0394547362162a0b37
Rely Ky <relyky@hotmail.com>
2023-12-08 12:38:33 +0800

    countup

--- src/pages/ReactFlowLab3/CustomNode.tsx
+++ src/pages/ReactFlowLab3/CustomNode.tsx
@@ -1,11 +1,22 @@
-import { useCallback } from 'react'
-import type { ChangeEvent } from 'react'
+import { useEffect, useRef } from 'react'
 import { Handle, Position } from 'reactflow'
 import type { NodeProps } from 'reactflow'
+import { useCountUp } from 'react-countup'
 import styles from './CustomNode.module.css'
 
 export default function CustomNode(props: NodeProps) {
     const { data } = props
+
+    const countUpRef = useRef(null)
+    const { update } = useCountUp({
+        ref: countUpRef,
+        start: 0,
+        end: 1000,
+        duration: 1
+    })
+
+    useEffect(() => update(data.amount)
+        , [data])
 
     return (
         <div className={styles.custom_node}>
@@ -14,7 +25,8 @@ export default function CustomNode(props: NodeProps) {
                 isConnectable={props.isConnectable} />
             <div>
                 <label>{data.label ?? 'unknow'}</label>
-                <p>{data.amount}</p>
+                {/* <p>{data.amount}</p> */}
+                <p ref={countUpRef} />
             </div>
             <Handle
                 type="source"
