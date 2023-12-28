18087728214b7e69841e79bf91dae95ef0cd8de5
Rely Ky <relyky@hotmail.com>
2023-12-28 11:56:37 +0800

    TradingViewWidget - AdvancedChartWidget

--- src/pages/TradingViewLab/AppForm.tsx
+++ src/pages/TradingViewLab/AppForm.tsx
@@ -1,18 +1,22 @@
 import { Box, Container, Typography } from '@mui/material'
 import TradingViewWidget from './TradingViewWidget'
-import { blue } from '@mui/material/colors'
+import AdvancedChartWidget from './AdvancedChartWidget'
 
 export default function TradingViewLab_AppForm() {
   return (
     <Container>
       <Typography variant='h3'>Trading View Lab</Typography>
 
-      <Box sx={{overflow:'hidden'}}>
-        <TradingViewWidget />
+      <Box sx={{ border: 2, borderColor: 'error.main', width: 600, height:300 }}>
+        <AdvancedChartWidget />
       </Box>
 
+      {/* <Box sx={{overflow:'hidden'}}>
+        <TradingViewWidget />
+      </Box> */}
+
       {/* tail */}
-      <Box sx={{ mb: 3, pb:3, backgroundColor: 'blue' }}>TAIL</Box>
+      <Box sx={{ mb: 3, pb: 3, backgroundColor: 'blue' }}>TAIL</Box>
     </Container>
   )
 }
